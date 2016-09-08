36ed9dd68f4ed3a1965be2ce5c7c4a593245e7d0
Jon Gauthier <jon@gauthiers.net>
2016-09-07 17:43:00 -0700

    RNN model: also build a single-step graph so that we can support rllab

--- webnav/rnn_model.py
+++ webnav/rnn_model.py
@@ -17,8 +17,32 @@ def score_beam(state, candidates):
     return scores
 
 
+def make_cell_state_placeholder(cell, name):
+    if isinstance(cell.state_size, int):
+        return tf.placeholder(tf.float32, shape=(None, cell.state_size),
+                              name=name)
+    elif isinstance(cell.state_size, tuple):
+        return tuple([tf.placeholder(tf.float32, shape=(None, size_i),
+                                     name="%s/cell_%i" % (name, i))
+                      for i, size_i in enumerate(cell.state_size)])
+    else:
+        raise ValueError("Unknown cell state size declaration %s"
+                         % cell.state_size)
+
+
+def make_cell_zero_state(cell, batch_size):
+    if isinstance(cell.state_size, int):
+        return np.zeros((batch_size, cell.state_size), dtype=np.float32)
+    elif isinstance(cell.state_size, tuple):
+        return tuple([np.zeros((batch_size, state_i))
+                      for state_i in cell.state_size])
+    else:
+        raise ValueError("Unknown cell state size declaration %s"
+                         % cell.state_size)
+
+
 def rnn_model(beam_size, num_timesteps, embedding_dim, cells=None,
-              name="model"):
+              build_single_step_graph=False, name="model"):
     with tf.variable_scope(name):
         if cells is None:
             cells = [tf.nn.rnn_cell.BasicLSTMCell(1024, state_is_tuple=True)]
@@ -39,14 +63,17 @@ def rnn_model(beam_size, num_timesteps, embedding_dim, cells=None,
 
         # Run stacked RNN.
         batch_size = tf.shape(current_nodes[0])[0]
-        hid_vals = [[cell.zero_state(batch_size, tf.float32)
-                     for cell in cells]]
+        inputs = [tf.concat(1, [current_nodes_t, query])
+                  for current_nodes_t in current_nodes]
+        hid_vals = [cell.zero_state(batch_size, tf.float32)
+                    for cell in cells]
         scores = []
-        for t in range(num_timesteps):
-            if t > 0: tf.get_variable_scope().reuse_variables()
 
-            inp = tf.concat(1, [current_nodes[t], query])
-            hid_prev_t = hid_vals[-1]
+        def step(hid_prev_t, input_t):
+            """
+            Build a single vertical-unrolled step in the RNN graph.
+            """
+            inp = input_t
             hid_t = []
             for i, (cell, hid_prev) in enumerate(zip(cells, hid_prev_t)):
                 inp, hid_t_i = cell(inp, hid_prev, scope="layer%i" % i)
@@ -62,9 +89,31 @@ def rnn_model(beam_size, num_timesteps, embedding_dim, cells=None,
                         scope="state_projection")
 
             scores_t = score_beam(last_out, candidates[t])
+
+            return scores_t, hid_t
+
+        for t in range(num_timesteps):
+            if t > 0: tf.get_variable_scope().reuse_variables()
+
+            scores_t, hid_vals = step(hid_vals, inputs[t])
             scores.append(scores_t)
+
+        # Also build a single-step graph.
+        tf.get_variable_scope().reuse_variables()
+        hid_prev_single = [
+                make_cell_state_placeholder(cell_i, "hid_prev_%i" % i)
+                for i, cell_i in enumerate(cells)]
+        input_single = inputs[0]
+        scores_single, hid_single = step(hid_prev_single, input_single)
 
     inputs = (current_nodes, query, candidates)
     outputs = (scores,)
-    return inputs, outputs
+
+    single_inputs = (current_nodes[0], query, candidates[0], hid_prev_single)
+    single_outputs = (scores_single, hid_single)
+
+    if build_single_step_graph:
+        return (inputs, outputs), (single_inputs, single_outputs)
+    else:
+        return inputs, outputs
 
