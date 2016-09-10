69b681f43fffdee149b24668860d40fd1100ec56
Jon Gauthier <jon@gauthiers.net>
2016-09-10 12:42:23 -0700

    Factor out q_model into a separate model file

--- webnav/scripts/train_qlearning.py
+++ webnav/scripts/train_qlearning.py
@@ -17,72 +17,9 @@ from tqdm import tqdm, trange
 
 from webnav import web_graph
 from webnav.environments import EmbeddingWebNavEnvironment
-from webnav.rnn_model import rnn_model
+from webnav.rnn_model import q_model
 from webnav.session import PartialRunSessionManager
 from webnav.util import discount_cumsum
-
-
-RLModel = namedtuple("RLModel",
-                     ["current_node", "query", "candidates",
-                      "scores",
-                      "rewards", "masks",
-                      "all_losses", "loss"])
-
-
-def q_model(beam_size, num_timesteps, embedding_dim, gamma=0.99,
-            name="model"):
-    # The Q-learning model uses the RNN scoring function as the
-    # Q-function.
-    rnn_inputs, rnn_outputs = rnn_model(beam_size, num_timesteps,
-                                        embedding_dim, name=name)
-    current_node, query, candidates = rnn_inputs
-    scores, = rnn_outputs
-
-    # Per-timestep non-discounted rewards
-    rewards = [tf.placeholder(tf.float32, (None,), name="rewards_%i" % t)
-               for t in range(num_timesteps)]
-    # Mask learning for those examples which stop early
-    masks = [tf.placeholder(tf.float32, (None,), name="mask_%i" % t)
-             for t in range(num_timesteps)]
-
-    # Calculate Q targets.
-    q_targets = []
-    q_a_pred = [tf.reduce_max(scores_t, 1) for scores_t in scores]
-    for t in range(num_timesteps):
-        target = rewards[t]
-        if t < num_timesteps - 1:
-            # Bootstrap with max_a Q_{t+1}
-            target += gamma * q_a_pred[t + 1]
-
-        q_targets.append(target)
-
-        # Summary: mean and variance of Q targets at this timestep
-        targets_mean, targets_var = tf.nn.moments(target, [0])
-        tf.scalar_summary("q/%i/target/mean" % t, targets_mean)
-        tf.scalar_summary("q/%i/target/var" % t, targets_var)
-
-        # Summary: mean and variance of predicted Q values for optimal
-        # actions at this timestep
-        q_a_pred_mean, q_a_pred_var = tf.nn.moments(q_a_pred[t], [0])
-        tf.scalar_summary("q/%i/pred_max/mean" % t, q_a_pred_mean)
-        tf.scalar_summary("q/%i/pred_max/var" % t, q_a_pred_var)
-
-        # Summary: mean and variance of all Q values in batch
-        q_pred_mean, q_pred_var = tf.nn.moments(scores[t], [0, 1])
-        tf.scalar_summary("q/%i/pred/mean" % t, q_pred_mean)
-        tf.scalar_summary("q/%i/pred/var" % t, q_pred_var)
-
-    losses = [tf.reduce_mean(tf.square(mask_t * (q_target_t - q_a_pred_t)))
-              for q_target_t, q_a_pred_t, mask_t
-              in zip(q_targets, q_a_pred, masks)]
-    loss = tf.add_n(losses) / float(len(losses))
-
-    tf.scalar_summary("loss", loss)
-
-    return RLModel(current_node, query, candidates,
-                   scores,
-                   rewards, masks,
-                   losses, loss)
 
 
 def rollout(model, env, sm, sess, args):
