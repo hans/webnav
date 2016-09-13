59404e4c4eceb2dace848e3fc0939969d6726392
Jon Gauthier <jon@gauthiers.net>
2016-09-12 18:43:00 -0700

    Serious evaluation for greedy model

--- webnav/scripts/greedy_comm.py
+++ webnav/scripts/greedy_comm.py
@@ -17,28 +17,36 @@ from webnav.util import log_trajectory
 def rollout(env, args, beta=1.0):
     observation = env.reset()
     wrapped_env = env._env
+    navigator = wrapped_env._navigator
+    graph = wrapped_env._graph
 
     trajectory = [(WRAPPED, (0, wrapped_env.cur_article_id), 0.0)]
     target = env._env._navigator.target_id
+    steps_to_target = None
 
     for t in range(args.path_length):
         (query, current_node, beam), message = observation
 
-        scores = np.dot(beam, query)
-        scores /= np.linalg.norm(beam, axis=1)
-        scores /= np.linalg.norm(query)
-        action = scores.argmax()
+        if navigator.cur_article_id == target:
+            action = [action for action in range(args.beam_size)
+                      if navigator._beam[action] == graph.stop_sentinel][0]
+            steps_to_target = t
+        else:
+            scores = np.dot(beam, query)
+            scores /= np.linalg.norm(beam, axis=1)
+            scores /= np.linalg.norm(query)
+            action = scores.argmax()
 
         next_step = env.step(action)
         observation = next_step.observation
 
-        traj_data = (action, wrapped_env.get_article_for_action(action))
+        traj_data = (action, wrapped_env.cur_article_id)
         trajectory.append((WRAPPED, traj_data, next_step.reward))
 
         if next_step.done:
             break
 
-    return trajectory, target
+    return trajectory, target, steps_to_target
 
 
 if __name__ == "__main__":
@@ -46,17 +54,9 @@ if __name__ == "__main__":
 
     p.add_argument("--path_length", default=3, type=int)
     p.add_argument("--beam_size", default=10, type=int)
-    p.add_argument("--batch_size", default=1, type=int)
-    p.add_argument("--learning_rate", default=0.001, type=float)
-    p.add_argument("--gamma", default=0.99, type=float)
-
-    p.add_argument("--n_epochs", default=3, type=int)
-    p.add_argument("--n_eval_iters", default=2, type=int)
+    p.add_argument("--n_rollouts", default=1000, type=int)
 
     p.add_argument("--logdir", default="/tmp/webnav_q_comm")
-    p.add_argument("--eval_interval", default=100, type=int)
-    p.add_argument("--summary_interval", default=100, type=int)
-    p.add_argument("--n_eval_trajectories", default=5, type=int)
 
     p.add_argument("--data_type", choices=["wikinav", "wikispeedia"],
                    default="wikinav")
@@ -65,9 +65,19 @@ if __name__ == "__main__":
     p.add_argument("--emb_path", required=True)
 
     args = p.parse_args()
+    args.batch_size = 1
 
     graph, envs, eval_envs = build_webnav_conversation_envs(args)
-
     log_f = sys.stdout
-    traj, target = rollout(envs[0], args)
-    log_trajectory(traj, target, envs[0], log_f)
+
+    n, successes = 0, []
+    for _ in xrange(args.n_rollouts):
+        traj, target, steps_to_target = rollout(envs[0], args)
+        n += 1
+        if steps_to_target is not None:
+            successes.append(steps_to_target)
+
+        # log_trajectory(traj, target, envs[0], log_f)
+
+    print "Success: %f%%" % (len(successes) / float(n) * 100)
+    print "Mean # steps to success: %f" % np.mean(successes)
