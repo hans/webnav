82d9173ebc4bc81113bd26828833c1c9f5554280
Jon Gauthier <jon@gauthiers.net>
2016-09-21 16:30:32 -0700

    Line up debug log columns

--- webnav/util.py
+++ webnav/util.py
@@ -101,7 +101,7 @@ def log_trajectory(trajectory, target, env, log_f):
         desc_fmt = "\t%-40s\t%.5f"
         if "nav_mean" in tags:
             # Log moments of navigation distributions
-            desc_fmt = "\t%%-40s\t%%.5f\t\t%+.5f\t%+.5f" % \
+            desc_fmt = "\t%%-40s\t%%+.5f\t\t%+.5f\t%+.5f" % \
                     (tags["nav_mean"], tags["nav_var"])
 
         tqdm.write(desc_fmt % (desc, reward), log_f)
